dc50597d3425f20207d4820d04052a46dae0fe8f
hydai <hydai@secondstate.io>
2020-07-15 10:30:07 +0000

    [Wasi] Load wasi options when initialize ssvm-napi

--- ssvmaddon.h
+++ ssvmaddon.h
@@ -4,6 +4,7 @@
 #include "vm/configure.h"
 #include "vm/vm.h"
 #include "host/wasi/wasimodule.h"
+#include "common/statistics.h"
 #include <napi.h>
 #include <string>
 #include <vector>
@@ -30,6 +31,7 @@ private:
   SSVM::VM::Configure *Configure;
   SSVM::VM::VM *VM;
   SSVM::Runtime::Instance::MemoryInstance *MemInst;
+  SSVM::Statistics::Statistics Stat;
   SSVM::Host::WasiModule *WasiMod;
   bool WBMode;
   bool Inited;
@@ -39,14 +41,16 @@ private:
   std::vector<uint8_t> InputBytecode;
   std::vector<uint8_t> ResultData;
   std::vector<std::string> ArgsVec;
-  Napi::Object WasiOptions;
+  std::vector<std::string> WasiCmdArgs, WasiDirs, WasiEnvs;
 
   std::unordered_map<std::size_t, std::string> CodeCache;
 
   /// Setup related functions
   void InitVM(const Napi::CallbackInfo &Info);
   /// Prepare wasi module from given wasi options
-  void PrepareWasi(const Napi::CallbackInfo &Info, SSVM::Host::WasiModule *WasiMod, const Napi::Object &WasiOptions, const std::string &FuncName);
+  bool parseCmdArgs(std::vector<std::string> &CmdArgs, const Napi::Object &Options);
+  bool parseDirs(std::vector<std::string> &Dirs, const Napi::Object &Options);
+  bool parseEnvs(std::vector<std::string> &Envs, const Napi::Object &Options);
   /// WasmBindgen related functions
   void EnableWasmBindgen(const Napi::CallbackInfo &Info);
   void PrepareResourceWB(const Napi::CallbackInfo &Info,
@@ -56,6 +60,8 @@ private:
   Napi::Value RunInt(const Napi::CallbackInfo &Info);
   Napi::Value RunString(const Napi::CallbackInfo &Info);
   Napi::Value RunUint8Array(const Napi::CallbackInfo &Info);
+  /// Statistics
+  Napi::Value GetStatistics(const Napi::CallbackInfo &Info);
 #if 0
   /// Wasi related functions
   void PrepareResource(const Napi::CallbackInfo &Info);
