570a575446742a43b414cd055914f55c9a310903
hydai <hydai@secondstate.io>
2020-07-21 11:00:32 +0000

    [Release] Version 0.4.6: Check libstdc++ version on aarch64 and x86_64 targets

--- ssvmaddon.cc
+++ ssvmaddon.cc
@@ -19,10 +19,31 @@ Napi::Object SSVMAddon::Init(Napi::Env Env, Napi::Object Exports) {
   Napi::HandleScope Scope(Env);
 
 #ifdef __GLIBCXX__
-  if (__GLIBCXX__ < 20200422) {
+#ifdef __aarch64__
+  std::string GLibPath = "/usr/lib/aarch64-linux-gnu";
+#endif
+#ifdef __x86_64__
+  std::string GLibPath = "/usr/lib/x86_64-linux-gnu";
+#endif
+  std::string GLibCXXName = "libstdc++.so.6.0.";
+  std::string CurrentGLibVer = "unknown";
+  bool IsVersionCompatible = false;
+  for (const auto & Entry : std::filesystem::directory_iterator(GLibPath)) {
+    std::string LibName = Entry.path().filename().string();
+    size_t Pos = LibName.find(GLibCXXName);
+    if (Pos != std::string::npos) {
+      CurrentGLibVer = LibName;
+      std::string GV = LibName.substr(GLibCXXName.length(), LibName.length());
+      if (std::stoi(GV) >= 29) {
+        IsVersionCompatible = true;
+      }
+    }
+  }
+  if (!IsVersionCompatible) {
     std::cerr << "====================================================================\n"
       << "Error: libstdc++ version mismatched!\n"
-      << "SSVM relies on >libstdc++6.0.28 (GLIBCXX >= 3.4.28)\n"
+      << "Your current version is " << CurrentGLibVer << " which is less than libstdc++6.0.28\n"
+      << "SSVM relies on >=libstdc++6.0.28 (GLIBCXX >= 3.4.28)\n"
       << "Please upgrade the libstdc++6 library.\n\n"
       << "For more details, refer to our environment set up document: https://www.secondstate.io/articles/setup-rust-nodejs/\n"
       << "====================================================================\n";
