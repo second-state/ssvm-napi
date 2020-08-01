982abbc672a82df5c32bd352fcca4f5b5de3ac50
hydai <hydai@secondstate.io>
2020-08-01 03:50:32 +0000

    [Utils] Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+ARCH=$1
 CXX=g++-9 node-pre-gyp install --fallback-to-build --update-binary
 rm -rf ssvm
 mkdir ssvm
 cp build/Release/ssvm.node ssvm
 strip ssvm/ssvm.node
-tar zcvf ssvm-linux-x64.tar.gz ssvm
+tar zcvf ssvm-linux-${ARCH}.tar.gz ssvm
