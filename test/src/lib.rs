40d9f5634be78145051ff466f7e302706ad2e609
Yi Huang <yi@secondstate.io>
2020-09-14 17:36:45 +0000

    [Test] Add tests for u32, s64, and u64

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -6,3 +6,21 @@ pub fn lcm_s32(a: i32, b: i32) -> i32 {
   let r = lcm(a, b);
   return r;
 }
+
+#[wasm_bindgen]
+pub fn lcm_u32(a: u32, b: u32) -> u32 {
+  let r = lcm(a, b);
+  return r;
+}
+
+#[wasm_bindgen]
+pub fn lcm_s64(a: i64, b: i64) -> i64 {
+  let r = lcm(a, b);
+  return r;
+}
+
+#[wasm_bindgen]
+pub fn lcm_u64(a: u64, b: u64) -> u64 {
+  let r = lcm(a, b);
+  return r;
+}
