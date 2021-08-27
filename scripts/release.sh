14bd20b4bb69376420947a4100d8e3d371cec8c1
dm4 <dm4@secondstate.io>
2021-08-27 17:20:12 +0800

    [Script] Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 ARCH=$1
-CXX=g++-9 node-pre-gyp install --fallback-to-build --update-binary
-rm -rf ssvm
-mkdir ssvm
-cp build/Release/ssvm.node ssvm
-strip ssvm/ssvm.node
-tar zcvf ssvm-linux-${ARCH}.tar.gz ssvm
+npm run release
+rm -rf wasmedge
+mkdir wasmedge
+cp build/Release/wasmedge.node wasmedge
+strip wasmedge/wasmedge.node
+tar zcvf wasmedge-linux-${ARCH}.tar.gz wasmedge
